755b2e73acf17391bc66d760c7861af0ef40ad7f
lexisvon <LexisvonSeggern@Lexiss-MacBook-Pro.local>
2017-02-20 15:08:50 -0600

    js mobile nav fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+$('.mobile-nav-btn').click(function(e){
+		$('.mobile-nav > ul').slideToggle();
+		// this prevents the browser from doing the default link action
+		e.preventDefault();
+	});
 
+	var $window = $(window);
+	  $window.on('resize', function (){
+        if ($window.width() < 600)
+        {
+            $('.mobile-nav').show();
+        }
+    });
 });
