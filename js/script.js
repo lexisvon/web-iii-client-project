b87c35b37811d007aded1b540ba58c74e863de0e
evjwood <evjwood@gmail.com>
2017-03-09 16:06:35 -0600

    added gretchen's updated jquery

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
 
-	$('.mobile-nav-btn').click(function(){
+	$('.mobile-nav-btn').click(function(e){
 		$('.mobile-nav-btn').removeClass('display');
 		$('.mobile-nav-btn-active').removeClass('hide');
 		$('.mobile-nav-btn').addClass('hide');
 		$('.mobile-nav-btn-active').addClass('display');
 		var mobileNav = $(this).attr("class");
 		console.log(mobileNav);
+
+		$(".mobile-nav").addClass("show");
+
+		e.preventDefault();
 	})
 
-	$('.mobile-nav-btn-active').click(function(){
+	$('.mobile-nav-btn-active').click(function(e){
 
 		$('.mobile-nav-btn').removeClass('hide');
 		$('.mobile-nav-btn-active').removeClass('display');
@@ -18,40 +22,25 @@ $(document).ready(function(){
 		$('.mobile-nav-btn-active').addClass('hide');
 		var mobileNavActive = $(this).attr("class");
 		console.log(mobileNavActive);
+
+		$(".mobile-nav").removeClass("show");
+
+		// this prevents the browser from doing the default link action
+		e.preventDefault();
 	})
-
-
-$('.mobile-nav-btn').click(function(e){
-		 $(".mobile-nav").addClass("show");
-
-		// this prevents the browser from doing the default link action
-		e.preventDefault();
-
-	});
-
-$('.mobile-nav-btn-active').click(function(e){
-		 $(".mobile-nav").removeClass("show");
-
-		// this prevents the browser from doing the default link action
-		e.preventDefault();
-
-	});
 
 var $window = $(window);
 	  $window.on('resize', function (){
         if ($window.width() < 800){
             $('.mobile-nav-btn.display').show();
-            // $('.mobile-nav-btn-active.hide').hide();
-            // $('.mobile-nav-btn-active.display').show();
-            // $('mobile-nav').hide();
             $('.mobile-nav.show').show();
 
         } if ($window.width() > 801){
             $('.mobile-nav.show').removeClass('show');
-            $('.mobile-nav-btn.display').hide();
             $('.mobile-nav-btn').addClass('display');
             $('.mobile-nav-btn.display').hide();
             $('.mobile-nav-btn-active.display').removeClass('display');
+
         }
 		window.addEventListener('scroll', function () {
 		document.body.classList[
@@ -171,4 +160,3 @@ var $window = $(window);
 	});	  
 
 });
-
