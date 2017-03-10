fed62ba189538e0f566326c84b8e717b4c072bac
lexisvon <LexisvonSeggern@Lexiss-MacBook-Pro.local>
2017-03-10 15:10:56 -0600

    added smooth scroll. removed events nav label.

--- js/script.js
+++ js/script.js
@@ -48,6 +48,23 @@ var $window = $(window);
 		]('scrolled');
 		});
 });
+
+
+	  $('.smooth-scroll').on('click', 'a', function(e) {
+		// prevent default anchor click behavior
+		e.preventDefault();
+		// store hash
+		var hash = this.hash;
+		$('html, body').animate({
+		scrollTop: $(this.hash).offset().top-0
+		}, 700, function(){
+		// when done, add hash to url
+		// (default click behaviour)
+		window.location.hash = hash;
+			});
+		});
+
+
 		$('.gallery-2').slick({
 		 infinite: true,
 		speed: 500,
