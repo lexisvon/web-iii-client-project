75a45fb8e119de4816ec9612c0dfb9a11647d5b7
lexisvon <LexisvonSeggern@Lexiss-MacBook-Pro.local>
2017-03-13 15:52:16 -0500

    fixed nav scroll opacity

--- js/script.js
+++ js/script.js
@@ -43,13 +43,15 @@ var $window = $(window);
 
         }
 
-      window.addEventListener('scroll', function () {
+      
+
+});
+
+	  window.addEventListener('scroll', function () {
 	  document.body.classList[
 	  window.scrollY > 20 ? 'add': 'remove'
 	  ]('scrolled');
 	  });
-
-});
 
 
 
